f5f59d8aee1b476721e9840d4a0aa0a4b8befac8
Lucakurotaki <61321906+Lucakurotaki@users.noreply.github.com>
2022-04-22 21:55:23 -0300

    Update albumListItem.dart

--- PDM 07 Exercícios/PDM 07 Exercícios - desafio/albumListItem.dart
+++ PDM 07 Exercícios/PDM 07 Exercícios - desafio/albumListItem.dart
@@ -2,7 +2,6 @@ import 'album.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-/// List item representing a single Character with its photo and name.
 class AlbumListItem extends StatelessWidget {
   const AlbumListItem({
     required this.album,
