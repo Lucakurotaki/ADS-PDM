c934861e9ffefcbfd69cd0ff36b6e3e57cea7b4b
Luca Kurotaki <lucakurotaki12@gmail.com>
2022-06-18 02:12:47 -0300

    Data sources are implemented

--- PDM 10 Exercícios/lib/features/ghibliFilm/data/datasources/ghibli_film_local_data_source.dart
+++ PDM 10 Exercícios/lib/features/ghibliFilm/data/datasources/ghibli_film_local_data_source.dart
@@ -1,7 +1,35 @@
+import 'dart:convert';
+
+import 'package:number_trivia/core/error/exception.dart';
 import 'package:number_trivia/features/ghibliFilm/data/models/ghibli_film_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class GhibliFilmLocalDataSource{
   Future<GhibliFilmModel> getLastGhibliFilm();
 
   Future<void> cacheGhibliFilm(GhibliFilmModel filmToCache);
+}
+
+const cachedGhibliFilm = "CACHED_GHIBLI_FILM";
+
+class GhibliFilmLocalDataSourceImpl implements GhibliFilmLocalDataSource{
+  final SharedPreferences sharedPreferences;
+
+  GhibliFilmLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<GhibliFilmModel> getLastGhibliFilm(){
+    final jsonString = sharedPreferences.getString(cachedGhibliFilm);
+
+    return jsonString != null ? Future.value(GhibliFilmModel.fromJson(jsonDecode(jsonString)))
+      : throw CacheException();
+  }
+
+
+  @override
+  Future<void> cacheGhibliFilm(GhibliFilmModel filmToCache){
+    return sharedPreferences.setString(
+      cachedGhibliFilm, 
+      jsonEncode(filmToCache.toString()));
+  }
 }
--- PDM 10 Exercícios/lib/features/ghibliFilm/data/datasources/ghibli_film_remote_data_source.dart
+++ PDM 10 Exercícios/lib/features/ghibliFilm/data/datasources/ghibli_film_remote_data_source.dart
@@ -1,8 +1,59 @@
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:number_trivia/core/error/exception.dart';
 import 'package:number_trivia/features/ghibliFilm/data/models/ghibli_film_model.dart';
 
 abstract class GhibliFilmRemoteDataSource {
   Future<GhibliFilmModel> getGhibliFilmById(String id);
 
   Future<GhibliFilmModel> getRandomGhibliFilm(String id);
+}
+
+class GhibliFilmRemoteDataSourceImpl implements GhibliFilmRemoteDataSource{
+  final http.Client client;
+
+  GhibliFilmRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<GhibliFilmModel> getGhibliFilmById(String id) =>
+    _getFilmFromUrl('https://https://ghibliapi.herokuapp.com/films/$id');
+
+  @override
+  Future<GhibliFilmModel> getRandomGhibliFilm(String id) =>
+    _getFilmFromUrl('https://https://ghibliapi.herokuapp.com/films/$id');
+  
+
+  Future<GhibliFilmModel> _getFilmFromUrl(String urlParam) async {
+    var url = Uri.parse(urlParam);
+
+    final response = await client.get(
+      url, headers: {'Content-Type' : 'application/json'},
+    );
+
+    return response.statusCode == 200 ? GhibliFilmModel.fromJson(jsonDecode(response.body))
+      : throw ServerException();
+  }
+  /**@override
+  Future<GhibliFilmModel> getGhibliFilmById(String id) async{
+    var url =  Uri.parse('https://https://ghibliapi.herokuapp.com/films/$id');
+
+    final response = await client.get(
+      url, headers: {'Content-Type' : 'application/json'},
+    );
+
+    return response.statusCode == 200 ? GhibliFilmModel.fromJson(jsonDecode(response.body))
+      : throw ServerException();
+  }
+
+  @override
+  Future<GhibliFilmModel> getRandomGhibliFilm(String id) async{
+    var url = Uri.parse('https://https://ghibliapi.herokuapp.com/film/$id');
+
+    final response = await client.get(
+      url, headers: {'Content-Type' : 'application/json'},
+    );
+
+    return response.statusCode == 200 ? GhibliFilmModel.fromJson(jsonDecode(response.body))
+      : throw ServerException();
+  }*/
 }
